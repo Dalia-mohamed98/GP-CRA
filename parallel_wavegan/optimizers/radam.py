cb749a5cc8db6706d3f6a61d984557148e162e6e
Mary_N <marynader256@gmail.com>
2020-08-18 00:35:23 +0200

    Mary's modules

--- parallel_wavegan/optimizers/radam.py
+++ parallel_wavegan/optimizers/radam.py
@@ -1,49 +1,37 @@
-# -*- coding: utf-8 -*-
-
-"""RAdam optimizer.
-
-This code is drived from https://github.com/LiyuanLucasLiu/RAdam.
-"""
-
 import math
-import torch
 
+import torch
 from torch.optim.optimizer import Optimizer
 
 
 class RAdam(Optimizer):
-    """Rectified Adam optimizer."""
+    def __init__(self,params,lr=1e-3,betas=(0.9,0.999),eps=1e-8,weight_decay=0):
+        
+        defaults = dict(lr=lr,betas=betas,eps=eps,weight_decay=weight_decay) #buffer[i][0] = step ,buffer[i][1] = Simple moving average length, buffer[i][0] = step_size
+        self.buffer = [[None,None,None] for ind in range(10)]
+        super(RAdam,self).__init__(params,defaults)
 
-    def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-8, weight_decay=0):
-        """Initilize RAdam optimizer."""
-        defaults = dict(lr=lr, betas=betas, eps=eps, weight_decay=weight_decay)
-        self.buffer = [[None, None, None] for ind in range(10)]
-        super(RAdam, self).__init__(params, defaults)
 
     def __setstate__(self, state):
-        """Set state."""
-        super(RAdam, self).__setstate__(state)
+        super(RAdam,self).__setstate__(state)
 
-    def step(self, closure=None):
-        """Run one step."""
+    def step(self,closure=None):
         loss = None
         if closure is not None:
             loss = closure()
 
         for group in self.param_groups:
-
             for p in group['params']:
-                if p.grad is None:
+                if p.grad is None :
                     continue
                 grad = p.grad.data.float()
                 if grad.is_sparse:
-                    raise RuntimeError('RAdam does not support sparse gradients')
+                    raise RuntimeError("RAdam doesn't support sparse gradients")
 
                 p_data_fp32 = p.data.float()
-
                 state = self.state[p]
 
-                if len(state) == 0:
+                if(len(state) == 0):
                     state['step'] = 0
                     state['exp_avg'] = torch.zeros_like(p_data_fp32)
                     state['exp_avg_sq'] = torch.zeros_like(p_data_fp32)
@@ -51,41 +39,78 @@ class RAdam(Optimizer):
                     state['exp_avg'] = state['exp_avg'].type_as(p_data_fp32)
                     state['exp_avg_sq'] = state['exp_avg_sq'].type_as(p_data_fp32)
 
-                exp_avg, exp_avg_sq = state['exp_avg'], state['exp_avg_sq']
-                beta1, beta2 = group['betas']
+                exp_avg,exp_avg_sq = state['exp_avg'],state['exp_avg_sq'] # DW , SW
+                beta1,beta2 = group['betas']
 
-                exp_avg_sq.mul_(beta2).addcmul_(1 - beta2, grad, grad)
-                exp_avg.mul_(beta1).add_(1 - beta1, grad)
-
-                state['step'] += 1
+                exp_avg.mul_(beta1).add_(1-beta1,grad) # DW = B1*DW+(1-B1)*grad
+                exp_avg_sq.mul_(beta2).addcmul_(1-beta2,grad,grad) # SW = B2*Sw + (1-B2)*grad*grad
+                state['step'] += 10 
                 buffered = self.buffer[int(state['step'] % 10)]
-                if state['step'] == buffered[0]:
-                    N_sma, step_size = buffered[1], buffered[2]
+
+                if(state['step'] == buffered[0]):
+                    N_sma,step_size = buffered[1],buffered[2] #N_sma = simple moving average length
                 else:
                     buffered[0] = state['step']
                     beta2_t = beta2 ** state['step']
+                    
+                    bias_c2 = 1 - beta2_t
+                    bias_c1 = 1 - beta1 ** state['step']
+
+                    # calculating N_SMA
                     N_sma_max = 2 / (1 - beta2) - 1
-                    N_sma = N_sma_max - 2 * state['step'] * beta2_t / (1 - beta2_t)
+                    N_sma  = N_sma_max - 2 * state['step'] * beta2_t / bias_c2
                     buffered[1] = N_sma
 
-                    # more conservative since it's an approximated value
+                    # calculating step_size
                     if N_sma >= 5:
-                        step_size = math.sqrt(
-                            (1 - beta2_t) * (N_sma - 4) / (N_sma_max - 4) * (N_sma - 2) / N_sma * N_sma_max / (N_sma_max - 2)) / (1 - beta1 ** state['step'])  # NOQA
+                        rect_term = math.sqrt(((N_sma-4)*(N_sma-2)*N_sma_max) / ((N_sma_max-4)*(N_sma_max-2)*N_sma))
+                        step_size = math.sqrt(bias_c2) * rect_term / bias_c1
+                       
+
+                  
                     else:
                         step_size = 1.0 / (1 - beta1 ** state['step'])
                     buffered[2] = step_size
 
                 if group['weight_decay'] != 0:
-                    p_data_fp32.add_(-group['weight_decay'] * group['lr'], p_data_fp32)
+                    p_data_fp32.add_(-group['weight_decay']*group['lr'],p_data_fp32)
 
-                # more conservative since it's an approximated value
-                if N_sma >= 5:
+                if N_sma >= 5: #apply rectified term
                     denom = exp_avg_sq.sqrt().add_(group['eps'])
-                    p_data_fp32.addcdiv_(-step_size * group['lr'], exp_avg, denom)
-                else:
-                    p_data_fp32.add_(-step_size * group['lr'], exp_avg)
-
+                    p_data_fp32.addcdiv_(-step_size * group['lr'],exp_avg,denom)
+                    
+                else: # SGD with momentum 
+                    p_data_fp32.add_(-step_size*group['lr'],exp_avg)
+    
                 p.data.copy_(p_data_fp32)
 
         return loss
+
+
+                    
+
+
+
+                    
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+                    
+
+
+
